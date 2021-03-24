c2a4a1f9aa3c38f89ee424fcff990a5816fd6be3
Bony Sylvester Carvalho <65407285+bonyscarvalho@users.noreply.github.com>
2021-03-23 20:36:38 -0400

    Update SearchRoatedArr.java

--- ChapterSortingAndSearching/SearchRoatedArr.java
+++ ChapterSortingAndSearching/SearchRoatedArr.java
@@ -2,15 +2,19 @@ package ChapterSortingAndSearching;
 
 public class SearchRoatedArr {
     public static void main(String args[]) {
-        int[] arr = {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14};
-        int search = 5;
-        System.out.println("Index of "+search+" is " + findElement(arr, search));
+        int[] arr =  {15, 16, 19, 20, 25, 27, 28, 4, 5, 7, 10, 14};                               //{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14};
+        int search = 28;
 
         int indx = findElementForDuplicates(arr, 0, arr.length -1, search);
         System.out.println("IDX: " + indx);
 
     }
 
+    //you start with normal increasing case PS every condition will have you search both left to mid-1 and mid+1 right
+    //1. a[left] < a[mid] if true you search for left side 1st where if val > a[left] && val < a[mid] --> true then left else right
+    //2. a[left] > a[mid] if true you search for right side 1st where if val > a[mid] && val <= a[right] --> true then right else left search
+    //3. (duplicates) a[left] == a[mid] if search right side 1st where a[mid] != a[right] --> true 
+    // then right search left 1st(with a variable to know if left is result or -1) if -1 then search right else return result. 
     private static int findElementForDuplicates(int[] arr, int leftIdx, int rightIdx, int searchVal) {
        // if(arr.length == 0) return -1;
 
@@ -19,19 +23,19 @@ public class SearchRoatedArr {
 
         if(rightIdx < leftIdx) return -1;
 
-        if(arr[leftIdx] < arr[mid]){    //Left is Increasing
+        if(arr[leftIdx] < arr[mid]){    //Left to mid is Increasing
             if(arr[leftIdx] <= searchVal && searchVal < arr[mid]){
                 return findElementForDuplicates(arr, leftIdx, mid - 1, searchVal );
             }else {
                 return findElementForDuplicates(arr, mid + 1, rightIdx, searchVal );
             }
-        }else if(arr[leftIdx] > arr[mid]){
+        }else if(arr[leftIdx] > arr[mid]){  //mid to left is decreasing
             if(arr[rightIdx] >= searchVal && arr[mid] < searchVal){
                 return findElementForDuplicates(arr, mid + 1, rightIdx, searchVal );
             }else {
                 return findElementForDuplicates(arr, leftIdx, mid - 1, searchVal );
             }
-        }else if(arr[leftIdx] == arr[mid]){
+        }else if(arr[leftIdx] == arr[mid]){     //both are equal
             if(arr[rightIdx] != arr[mid]){
                 return findElementForDuplicates(arr, mid + 1, rightIdx, searchVal );
             }else {
@@ -41,26 +45,6 @@ public class SearchRoatedArr {
                 }else {
                     return result;
                 }
-            }
-        }
-        return -1;
-    }
-
-    private static int findElement(int[] arr, int search) {
-        if(arr.length == 0) return -1;
-
-        int leftIdx = 0;
-        int rightIdx = arr.length - 1;
-
-        while (leftIdx <= rightIdx){
-            int midIdx = (leftIdx + rightIdx) / 2;
-
-            if(arr[midIdx] == search)return midIdx;
-
-            if(arr[rightIdx] < search){
-                rightIdx = midIdx - 1;
-            }else {
-                leftIdx = midIdx + 1;
             }
         }
         return -1;
